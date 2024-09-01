0cc7d8ca83f3bfe0078e9128593f7afd28f0ad0f
Gabriel Rezende <gabrielcastrorezende@gmail.com>
2024-09-01 12:32:16 -0300

    Sending email feature complete

--- email_sender.py
+++ email_sender.py
@@ -1,2 +1,26 @@
 import smtplib
-import ssl 
+from email.mime.text import MIMEText
+import os
+from dotenv import load_dotenv, dotenv_values
+
+subject = "Hello from Python"
+body = "This is a email body"
+
+sender = os.getenv("EMAIL_SENDER")
+
+recipient = ""
+
+sender_password = os.getenv("EMAIL_PASSWORD")
+
+def send_email(subject, body, sender, recipients, password):
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = recipients
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
+        smtp_server.login(sender, password)
+        smtp_server.sendmail(sender, recipients, msg.as_string())
+    print('Message sent')
+
+send_email(subject, body, sender,  recipient, sender_password)
