67013734fcf8b5fecd7d98a53ebefa1c42259842
Gabriel Rezende <gabrielcastrorezende@gmail.com>
2024-09-02 15:29:04 -0300

    Correção de bug

--- email_package/email_sender.py
+++ email_package/email_sender.py
@@ -9,20 +9,23 @@ PORT = os.getenv("PORT")
 SENDER_EMAIL = os.getenv("EMAIL_SENDER")
 PASSWORD = os.getenv("EMAIL_PASSWORD")
 
-subject = "E-mail automatico - teste"
+subject = "E-mail automatico - Concursos pelo Brasil"
 
-message = MIMEMultipart()
-message["From"] = SENDER_EMAIL
-message["Subject"] = subject
+def build_message(user):
+    message = MIMEMultipart()
+    message["From"] = SENDER_EMAIL
+    message["Subject"] = subject
+    message["To"] = user['email']
+    message.attach(MIMEText(retorna_email(user['name']), "html"))
 
+    return message
 
 def send_email(user):
     try: 
         server = smtplib.SMTP(SERVER_SMTP, PORT)
         server.starttls()
         server.login(SENDER_EMAIL, PASSWORD)
-        message["To"] = user['email']
-        message.attach(MIMEText(retorna_email(user['name']), "html"))
+        message = build_message(user)
         server.sendmail(SENDER_EMAIL, user['email'], message.as_string())
         print(f"Email enviado para {user['email']} com sucesso!")
     except Exception as e: 
